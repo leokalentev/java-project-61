6e2576889cec925a4183eae2924788ef11f4eef2
Leonid <leonigleo13@gmail.com>
2024-09-26 12:54:43 +0300

    Third step

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    application
+}
+
+application{
+    mainClass.set("hexlet.code.App")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
