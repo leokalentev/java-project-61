4f2ab076779275e90c4013dc1817e3173e28c024
Leonid <leonigleo13@gmail.com>
2024-10-06 17:11:38 +0300

    Third check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,33 @@
 plugins {
-    id("java")
     application
     checkstyle
-}
-
-application{
-    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application{
+    mainClass.set("hexlet.code.App")
+}
+
 repositories {
     mavenCentral()
 }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-
     implementation("org.projectlombok:lombok:1.18.28")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
-
-checkstyle {
-    toolVersion = "10.12.4"
-    configFile = file("config/checkstyle/checkstyle.xml")
-}
-
-tasks.withType<Checkstyle>().configureEach {
-    reports {
-        xml.required = true // Выключаем отчет в формате XML
-        html.required = true // Включаем отчет в формате HTML
-
-    }
-}
-
